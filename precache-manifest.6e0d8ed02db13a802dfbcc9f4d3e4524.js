d17008b1156a4c820a6e5f791961ae8a0d2f3bac
Farzan Hajian <fh791@yahoo.com>
2020-01-01 21:20:52 +0330

    Updates

---
+++ precache-manifest.6e0d8ed02db13a802dfbcc9f4d3e4524.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac3f6ccebcf4ca63383ea1223225ecd",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "8635b8e7c4123219c867",
+    "url": "/contactmanager/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "6c20a4aa38c1230124b6",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "8635b8e7c4123219c867",
+    "url": "/contactmanager/static/js/2.1966a1c0.chunk.js"
+  },
+  {
+    "revision": "c8336e0bcbf8ea1670aafb15aa044bf7",
+    "url": "/contactmanager/static/js/2.1966a1c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6c20a4aa38c1230124b6",
+    "url": "/contactmanager/static/js/main.38d6853b.chunk.js"
+  },
+  {
+    "revision": "329d56e5d2a3841584cb",
+    "url": "/contactmanager/static/js/runtime-main.220e9dd0.js"
+  }
+]);
