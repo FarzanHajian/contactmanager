704126928c82869aa467f9fae08b3db4ad350d38
Farzan Hajian <fh791@yahoo.com>
2020-01-01 23:43:30 +0330

    github pages

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <Provider>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <div className="App">
           <Header branding="Contact Manager" />
           <div className="container">
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,9 +9,9 @@ const Header = (props) => {
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
             <div className="container">
-                <a href="/" className="navbar-brand">
+                <Link to="/" className="navbar-brand">
                     {branding}
-                </a>
+                </Link>
                 <div>
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item">
